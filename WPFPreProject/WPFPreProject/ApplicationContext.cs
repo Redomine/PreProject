a487b38382416f764f8b44bc9b9bce1e84fb6e12
Redomine <romankaev@gmail.com>
2023-02-06 14:38:53 +0300

    Test

---
+++ WPFPreProject/WPFPreProject/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace WPFPreProject
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<building> buildings { get; set; }
+
+        public ApplicationContext() : base("DefaultConnection") { }
+    }
+}
